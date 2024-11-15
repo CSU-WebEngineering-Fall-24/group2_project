e9643f5f1a4f722e2634025eb5d76a58d9a7a677
adavidson21 <alexis.davidson221@gmail.com>
2024-11-14 20:40:14 -0500

    Update about container to display member data from separate json & add personal info.

--- src/main/react/containers/AboutContainer.jsx
+++ src/main/react/containers/AboutContainer.jsx
@@ -1,20 +1,21 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Member from "../components/Member";
+import membersData from "../../resources/members.json";
 
 const AboutContainer = () => {
+  const [members, setMembers] = useState([]);
 
-	const member = {
-		name: "Michael",
-		shortBio: "I am currently working on my Masters in Computer Science.  This is my last class and I'm very excited to graduate.  I started my career life as a draftsman in architecture.  In 2002, I went back to get my Bachelor's in Computer Science.  While working full time, I went to school part time and graduated in 2009.  I got a job at CallCopy in 2010 and have been a software engineer ever since.  I work at NICE, which is the number one vendor in Call Center as a Service (CCaaS).",
-		shortBioImageUrl: "https://avatars.githubusercontent.com/u/178439261?v=4"
-	};
+  useEffect(() => {
+    setMembers(membersData);
+  }, []);
 
-	return (
-		<>
-			About Page...
-			<Member member={member} />
-		</>
-	);
-}
+  return (
+    <>
+      {members.map((member) => (
+        <Member key={member.id} member={member} />
+      ))}
+    </>
+  );
+};
 
 export default AboutContainer;
