cfe9fcc72a3a7f03025a04ad1d7e4bca21a29c7a
Michael K. Sretenović <michael@sretenovic.org>
2024-11-24 15:44:10 -0500

    Issue #30 Create Jest test for Member.jsx and all tests are passing.

--- src/main/react/components/Member.jsx
+++ src/main/react/components/Member.jsx
@@ -4,13 +4,21 @@ const Member = (props) => {
 	const { member } = props;
 	return (
 		<div className="memberCard">
-			<div className="memberHeader">{member.name}</div>
-			<div className="memberBio">
-				{member.shortBioImageUrl &&
-					<img src={member.shortBioImageUrl} className="memberBioImageSm" />
-				}
-				<div className="memberBioText">{member.shortBio}</div>
-			</div>
+			{member && member.name
+				? <>
+					<div className="memberHeader">{member.name}</div>
+					<div className="memberBio">
+						{member.shortBioImageUrl &&
+							<img src={member.shortBioImageUrl} className="memberBioImageSm" />
+						}
+						<div className="memberBioText">{member.shortBio}</div>
+					</div>
+				</>
+				: <>
+					<div className="memberHeader">Unknown</div>
+					<div className="memberBio">No information found</div>
+				</>
+			}
 		</div>
 	);
 };
