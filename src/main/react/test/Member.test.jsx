d3064d0a9c21b3bd38144c118a2424757264c6ee
Alexis Davidson <alexis.davidson221@gmail.com>
2024-11-26 22:57:30 -0500

    Fix failing tests

--- src/main/react/test/Member.test.jsx
+++ src/main/react/test/Member.test.jsx
@@ -1,77 +1,55 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Member from "../components/Member";
 
 describe("Member", () => {
-	test("Render with no image", () => {
-		const info = {
-			name: "Michael",
-			shortBioImageUrl: null,
-			shortBio: "This is my short bio."
-		};
-		const member = render(<Member member={info} />);
-		const divCard = member.container.children[0];
-		const divHeader = divCard.children[0];
-		const divBio = divCard.children[1];
-		const divText = divBio.children[0];
+  test("Render with no image", () => {
+    const info = {
+      name: "Michael",
+      shortBioImageUrl: null,
+      shortBio: "This is my short bio.",
+    };
 
-		expect(divCard.classList.contains("memberCard")).toBe(true);
-		expect(divCard.children.length).toBe(2);
+    render(<Member member={info} />);
 
-		expect(divHeader.innerHTML).toBe(info.name);
-		expect(divText.innerHTML).toBe(info.shortBio);
-	});
+    expect(screen.getByText("Michael")).toBeInTheDocument();
+    expect(screen.getByText("This is my short bio.")).toBeInTheDocument();
+    expect(screen.queryByRole("img")).not.toBeInTheDocument();
+  });
 
-	test("Render with image", () => {
-		const info = {
-			name: "Michael",
-			shortBioImageUrl: "http://www.test.com",
-			shortBio: "This is my short bio."
-		};
-		const member = render(<Member member={info} />);
-		const divCard = member.container.children[0];
-		const divHeader = divCard.children[0];
-		const divBio = divCard.children[1];
-		const img = divBio.children[0];
-		const divText = divBio.children[1];
+  test("Render with image", () => {
+    const info = {
+      name: "Michael",
+      shortBioImageUrl: "http://www.test.com",
+      shortBio: "This is my short bio.",
+    };
 
-		expect(divCard.classList.contains("memberCard")).toBe(true);
-		expect(divCard.children.length).toBe(2);
+    render(<Member member={info} />);
 
-		expect(divHeader.innerHTML).toBe(info.name);
-		expect(img.getAttribute("src")).toBe(info.shortBioImageUrl);
-		expect(divText.innerHTML).toBe(info.shortBio);
-	});
+    expect(screen.getByText("Michael")).toBeInTheDocument();
+    expect(screen.getByText("This is my short bio.")).toBeInTheDocument();
+    expect(screen.getByRole("img")).toHaveAttribute(
+      "src",
+      "http://www.test.com"
+    );
+  });
 
-	test("Render with null member", () => {
-		const info = null;
-		const member = render(<Member member={info} />);
-		const divCard = member.container.children[0];
-		const divHeader = divCard.children[0];
-		const divBio = divCard.children[1];
+  test("Render with null member", () => {
+    render(<Member member={null} />);
+    expect(screen.getByText("Unknown")).toBeInTheDocument();
+    expect(screen.getByText("No information found")).toBeInTheDocument();
+  });
 
-		expect(divCard.classList.contains("memberCard")).toBe(true);
-		expect(divCard.children.length).toBe(2);
+  test("Render with member with null name", () => {
+    const info = {
+      name: null,
+      shortBioImageUrl: "http://www.test.com",
+      shortBio: "This is my short bio.",
+    };
 
-		expect(divHeader.innerHTML).toBe("Unknown");
-		expect(divBio.innerHTML).toBe("No information found");
-	});
+    render(<Member member={info} />);
 
-	test("Render with member with null name", () => {
-		const info = {
-			name: null,
-			shortBioImageUrl: "http://www.test.com",
-			shortBio: "This is my short bio."
-		};
-		const member = render(<Member member={info} />);
-		const divCard = member.container.children[0];
-		const divHeader = divCard.children[0];
-		const divBio = divCard.children[1];
-
-		expect(divCard.classList.contains("memberCard")).toBe(true);
-		expect(divCard.children.length).toBe(2);
-
-		expect(divHeader.innerHTML).toBe("Unknown");
-		expect(divBio.innerHTML).toBe("No information found");
-	});
+    expect(screen.getByText("Unknown")).toBeInTheDocument();
+    expect(screen.getByText("No information found")).toBeInTheDocument();
+  });
 });
