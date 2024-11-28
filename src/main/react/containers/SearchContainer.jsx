1dd5272bec06ef259ff0afc3e598f280c7c63ffe
Michael K. Sretenović <michael@sretenovic.org>
2024-11-27 19:02:11 -0500

    Updated pagination on the search results page to work correctly.

--- src/main/react/containers/SearchContainer.jsx
+++ src/main/react/containers/SearchContainer.jsx
@@ -6,19 +6,23 @@ import Pagination from "../components/Pagination";
 const SearchContainer = () => {
 	const location = useLocation();
 	const { searchInput, year, type, results = [] } = location.state || {};
-
-	const [currentPage, setCurrentPage] = useState(0);
 	const itemsPerPage = 15; // 3 rows of 5 movies
+	const [movies, setMovies] = useState(results);
+	const [currentPage, setCurrentPage] = useState(0);
 	const startIndex = currentPage * itemsPerPage;
+	const totalPages = Math.ceil(movies.length / itemsPerPage);
 
-	const totalPages = Math.ceil(results.length / itemsPerPage);
-	const paginatedResults = results.slice(startIndex, startIndex + itemsPerPage);
+	const paginatedResults = movies.slice(startIndex, startIndex + itemsPerPage);
 
 	useEffect(() => {
 		if (currentPage >= totalPages) {
 			setCurrentPage(0); // Reset to first page if out of bounds
 		}
 	}, [currentPage, totalPages]);
+
+	function onPaginationClick(pageNumber) {
+		setCurrentPage(pageNumber);
+	}
 
 	return (
 		<div className="container mt-4 search-container">
@@ -35,7 +39,7 @@ const SearchContainer = () => {
 				)}
 			</div>
 
-			{results.length === 0 ? (
+			{movies.length === 0 ? (
 				<p>No results found for your search.</p>
 			) : (
 				<>
@@ -47,10 +51,7 @@ const SearchContainer = () => {
 						))}
 					</div>
 					<div className="pagination-container mt-auto d-flex justify-content-center">
-						<Pagination
-							items={results}
-							onClick={setCurrentPage}
-						/>
+						<Pagination items={movies} onClick={onPaginationClick} />
 					</div>
 				</>
 			)}
